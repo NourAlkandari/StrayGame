e6b966bd18f408bce6e7a3bd26067be7803355ef
Nour Alkandari <nouralkandari@Nours-MacBook-Pro.local>
2019-04-14 20:36:25 +0300

    added initial model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 # Create your models here.
+class PetDescription(models.Model):
+	name = models.CharField(max_length=120)
+	breed = models.CharField(max_length=120)
+	weight = models.IntegerField()
+	age = models.IntegerField()
+
+class PetState(models.Model):
+	fun = models.IntegerField(default=1,
+        validators=[MaxValueValidator(5), MinValueValidator(1)])
+	social = models.IntegerField(default=1,
+        validators=[MaxValueValidator(5), MinValueValidator(1)])
+	hunger = models.IntegerField(default=1,
+        validators=[MaxValueValidator(5), MinValueValidator(1)])
+	sleep = models.IntegerField(default=1,
+        validators=[MaxValueValidator(5), MinValueValidator(1)])
+	bladder = models.IntegerField(default=1,
+        validators=[MaxValueValidator(5), MinValueValidator(1)])
+
+
+# User is the account (how to connect to pet??)
+# Need to connect pet states to pet
+
+# class Items(models.Model):
+# 	food = 
+# 	toys = 
+# 	health_kit = 
+# 	bed = 
+
+
