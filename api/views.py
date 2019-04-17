bc8cd3068a34be8848fb472dde1adb18af6cd0ae
sarah-am <sarahq.aman@gmail.com>
2019-04-17 20:03:53 +0300

    phase 2?

--- api/views.py
+++ api/views.py
@@ -20,8 +20,8 @@ class PetDetailView(RetrieveUpdateAPIView):
     # only one pet, therefore only one object, so no need for object_id
 
     def get_object(self):
-    	obj = Pet.objects.get(user=self.request.user)
-    	return obj
+        obj = Pet.objects.get(user=self.request.user)
+        return obj
 
 # Details of the pet state (dynamic-ish)
 class PetStateDetailView(RetrieveAPIView):
@@ -37,35 +37,52 @@ class FeedPet(APIView):
     serializer_class = PetStateSerializer
 
     def post(self, request):
-    	food_options = request.data
-    	pet = Pet.objects.get(user=request.user)
-    	states = pet.states
-    	if Food.options == Food.Dog_Food:
-    		states.hunger= max(0, states.hunger + double_counter) # need to decide what kind of metric to follow (e.g. if "hunger" is high then pet is full or is the pet hungry?)
-    	elif Food.options == Food.Chocolate:
-    		states.hunger= max(0, states.hunger - double_counter) # add effect on mood later on
-    	elif Food.options == Food.Todays_Lunch:
-    		states.hunger= max(0, states.hunger + counter)
-    	# OR below? Do I need the above if the serializer_class is already defined?
-    	# self.hunger = max(0, self.hunger - self.counter) #so you don't see negative hunger, duh!
-    	states.save()
-    	return Response(PetStateSerializer(pet.states).data) # self.hunger or state??
+        food = request.data.get("food") #"food" is the key from frontend. Setup an array in the frontend
+        pet = Pet.objects.get(user=self.request.user.id) #request.user only
+        states = pet.states
+# check the need level to cap it. Also add this validation in the front-end (user can't continue to feed pet if hunger is maxed out)
+# get the food option from the front end and check if it matches one of the food options (string). No need for a model for Food (would not work anyway)
+        if (states.hunger < 5) and (states.hunger >= 0):
+            if food == "Dog Food":
+                states.hunger= max(0, states.hunger + double_counter) # need to decide what kind of metric to follow (e.g. if "hunger" is high then pet is full or is the pet hungry?)
+            if food == "Chocolate":
+                states.hunger= max(0, states.hunger - double_counter) # add effect on mood later on
+            if food == "Today's Lunch":
+                states.hunger= max(0, states.hunger + counter)
+        # OR below? Do I need the above if the serializer_class is already defined?
+        # self.hunger = max(0, self.hunger - self.counter) #so you don't see negative hunger, duh!
+        states.save()
+        return Response(PetStateSerializer(pet.states).data) # self.hunger or state??
 
 class EntertainPet(APIView):
     serializer_class = PetStateSerializer
 
     def post(self, request):
-    	entertainment_options = request.data
-    	pet = Pet.objects.get(user=request.user)
-    	states = pet.states
-    	if Entertainment.options == Entertainment.Walk_Pet:
-    		states.hunger= max(0, states.hunger + double_counter) 
-    	elif Entertainment.options == Entertainment.Ignore:
-    		states.hunger= max(0, states.hunger - double_counter) 
-    	elif Entertainment.options == Entertainment.Go_to_Petstore:
-    		states.hunger= max(0, states.hunger + counter)
-    	states.save()
-    	return Response(PetStateSerializer(pet.states).data)
+        entertainment = request.data.get("entertainment")
+        pet = Pet.objects.get(user=request.user)
+        states = pet.states
+        if (states.fun < 5) and (states.fun >= 0):
+            if entertainment == "Walk Pet":
+                states.fun= max(0, states.fun + double_counter) 
+            elif entertainment == "Ignore":
+                states.fun= max(0, states.fun - double_counter) 
+            elif entertainment == "Go to Petstore":
+                states.fun= max(0, states.fun + counter)
+        states.save()
+        return Response(PetStateSerializer(pet.states).data)
+
+    # def post(self, request):
+    #     entertainment_options = request.data
+    #     pet = Pet.objects.get(user=request.user)
+    #     states = pet.states
+    #     if Entertainment.options == Entertainment.Walk_Pet:
+    #         states.hunger= max(0, states.hunger + double_counter) 
+    #     elif Entertainment.options == Entertainment.Ignore:
+    #         states.hunger= max(0, states.hunger - double_counter) 
+    #     elif Entertainment.options == Entertainment.Go_to_Petstore:
+    #         states.hunger= max(0, states.hunger + counter)
+    #     states.save()
+    #     return Response(PetStateSerializer(pet.states).data)
 
 ###############################################
 
