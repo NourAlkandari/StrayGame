18cc1e0e52be6a140dffa4f5fbed384af6988692
sarah-am <sarahq.aman@gmail.com>
2019-04-24 19:25:56 +0300

    fixed registration

--- api/serializers.py
+++ api/serializers.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Pet, PetState, Food, Entertainment
 import datetime
+from rest_framework_jwt.settings import api_settings
+
 # from django.utils.timezone import now
 
 
@@ -27,10 +29,11 @@ class PetDetailSerializer(serializers.ModelSerializer):
 
 class UserCreateSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+    token = serializers.CharField(allow_blank=True, read_only=True)
 
     class Meta:
         model = User
-        fields = ['username', 'password', 'email']
+        fields = ['username', 'password', 'email','token']
 
     def create(self, validated_data):
         username = validated_data['username']
@@ -39,4 +42,12 @@ class UserCreateSerializer(serializers.ModelSerializer):
         new_user = User(username=username, email=email)
         new_user.set_password(password)
         new_user.save()
+        jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+        jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+
+        payload = jwt_payload_handler(new_user)
+        token = jwt_encode_handler(payload)
+
+        validated_data["token"] = token
         return validated_data
+
